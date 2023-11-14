9f05e414c4c757562bfcbdd3d4715766c18a8aea
František Šilhán <frantiseksilhann@gmail.com>
2023-11-14 17:45:47 +0100

    done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Counter from './components/counter'
+
+function App() {
+  return (
+    <>
+      <p>Stopwatch</p>
+      <Counter seconds={2} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+interface ICounter {
+  seconds: number
+}
+
+export const Counter: React.FC<ICounter> = ({ seconds }) => {
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [counter, setCounter] = useState<number>(0);
+
+  useEffect(() => {
+    let interval: number;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setCounter(x => x + seconds);
+      }, seconds * 1000);
+    }
+    return () => { if (interval !== undefined) clearInterval(interval)};
+  }, [isRunning, seconds]);
+
+
+  return (
+    <>
+      <p>{counter}</p>
+      <button onClick={() => { setIsRunning(x => !x) }}>{isRunning ? "stop" : "run"}</button>
+    </>
+  );
+}
+
+export default Counter;
